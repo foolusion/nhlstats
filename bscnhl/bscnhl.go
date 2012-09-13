a1cccb8a4a2e968982be8eca57d674cae00966f7
Andrew O'Neill <foolusion@gmail.com>
2012-09-13 09:38:21 -0400

    initial commit

---
+++ bscnhl/bscnhl.go
@@ -0,0 +1,14 @@
+package bscnhl
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/", root)
+}
+
+func root(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, BSC!")
+}
